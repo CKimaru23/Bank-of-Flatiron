c7b70f17f60bca6058c35d21bb2384fe45de0d00
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2023-01-16 15:14:19 +0300

    Search.js semantic ui

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function Search({handleSearch}) {
   return (
-    <div>
+    <div className="ui large fluid icon input">
       <input
         type="text"
-        placeholder="Search your Recent Transactions"
+        placeholder="Search your Transactions"
         onChange={handleSearch}
       />
-      <i></i>
+      <i className="circular search link icon"></i>
     </div>
   );
 }
